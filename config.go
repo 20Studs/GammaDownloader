8a79fa3dcd9924dc498c0a7106c8c6d36a889a66
20Studs <158521110+20Studs@users.noreply.github.com>
2024-02-02 16:31:40 -0500

    Create config.go

---
+++ config.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	url  string = "https://github.com/20Studs/Gamma/raw/main/Player.exe"
+	path string = "~/AppData/Roaming/Brick Hill/Player.exe"
+
+	versionUrl string = "https://raw.githubusercontent.com/20Studs/Gamma/main/VERSION.txt"
+	noVersion  string = "v0.0"
+)
